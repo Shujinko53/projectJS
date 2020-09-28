c3aa4f9c5eab919972c5938edf56653894f72607
BirraMoretti <shujinko53@yandex.ru>
2020-09-28 21:53:53 +0300

    training the objects and arrays

--- js.js
+++ js.js
@@ -1,7 +1,70 @@
 'use strict';
 
-function showFirstMessage(text) {
-  alert(text);
-}
+// function showFirstMessage(text) {
+//   alert(text);
+// }
 
-showFirstMessage('Hello Everybody!!!:)');
+// showFirstMessage('Hello Everybody!!!:)');
+
+/*------------------------------------------*/
+
+// function learnJS(lang, callback) {
+//   console.log("I learn " + lang);
+//   callback();
+// }
+
+// learnJS("Javascript", done);
+
+// function done() {
+//   console.log("I've done the third lesson");
+// }
+
+/*------------------------------------------*/
+
+// let options = {
+//   width: 1024,
+//   height: 1024,
+//   name: "test"
+// };
+
+// console.log(options.name);
+
+// options.bool = true;
+// console.log(options.bool);
+// options.colors = {
+//   border: "#000000",
+//   bg: "blu"
+// }
+
+// delete options.bool;
+
+// console.log(options);
+
+// for (let key in options) {
+//   console.log('Свойство ' + key + " имеет значение " + options[key]);
+// }
+
+// let arr = ["first", 2, 3, "four", 5];
+
+// // arr.pop();
+// // arr.push("5");
+// // arr.shift();
+// // arr.unshift("1");
+
+// arr.forEach(function(item, i, mass) {
+//   console.log(i + ": " + item + " (массив: " + mass + ")");
+// });
+
+// console.log(arr);
+
+// let mass = [1,3,4,6,7];
+
+// for (let key of mass) {
+//   console.log(key);
+// }
+
+let answer = prompt("", ""),
+    arr = [];
+
+arr = answer.split(',');
+console.log(arr);
