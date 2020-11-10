d55607b8806bc87c47030e745ec22d0ed203daaf
BirraMoretti <shujinko53@yandex.ru>
2020-11-10 22:40:41 +0300

    The training on creating the class

--- js.js
+++ js.js
@@ -160,3 +160,25 @@
 // let numbers = [2, 5, 7];
 
 // log(...numbers);
+
+// The training on creating the class
+
+class Options {
+    constructor(height, width, bg, fontSize, textAlign) {
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+        this.textAlign = textAlign;
+    }
+    createDiv() {
+        let elem = document.createElement('div');
+        document.body.appendChild(elem);
+        let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+        elem.style.cssText = param;
+    }
+}
+
+const item = new Options(300, 500, "#d3d3d3", 14, "center");
+
+item.createDiv();
